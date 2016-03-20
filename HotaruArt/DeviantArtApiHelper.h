5aaf0868ab7033704e7c3ce95086d864acd67c47
Elena <madlax19@mail.ru>
2016-03-20 17:00:25 +0200

    project structure changed

---
+++ HotaruArt/DeviantArtApiHelper.h
@@ -0,0 +1,13 @@
+//
+//  DeviantArtApiHelper.h
+//  HotaruArt
+//
+//  Created by Elena on 20.03.16.
+//  Copyright © 2016 Elena. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface DeviantArtApiHelper : NSObject
+
+@end
---
+++ HotaruArt/DeviantArtApiHelper.m
@@ -0,0 +1,13 @@
+//
+//  DeviantArtApiHelper.m
+//  HotaruArt
+//
+//  Created by Elena on 20.03.16.
+//  Copyright © 2016 Elena. All rights reserved.
+//
+
+#import "DeviantArtApiHelper.h"
+
+@implementation DeviantArtApiHelper
+
+@end
