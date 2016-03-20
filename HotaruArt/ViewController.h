f6d07df0213dd42493b4001530878597e5100e63
Elena <madlax19@mail.ru>
2016-03-20 16:16:11 +0200

    first commit

---
+++ HotaruArt/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HotaruArt
+//
+//  Created by Elena on 20.03.16.
+//  Copyright © 2016 Elena. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ HotaruArt/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  HotaruArt
+//
+//  Created by Elena on 20.03.16.
+//  Copyright © 2016 Elena. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
